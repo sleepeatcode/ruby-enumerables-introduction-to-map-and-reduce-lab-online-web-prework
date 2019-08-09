572b90c6f67118d919b4fafc48e2dc7c8760a57d
Rachel T <rachmaybe@gmail.com>
2019-08-09 16:46:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,11 +35,11 @@ def reduce_to_any_true(source_array)
   counter = 0 
   while counter > source_array.length do
     if source_array[counter]
-      return false
+      return true
     end
     counter +=1
   end 
-  return true
+  return false
 end 
 
 
